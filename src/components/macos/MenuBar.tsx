2a688239e0bccffb90c73e0307aa35ba7423fb85
Thanas R <thanas5.rd@gmail.com>
2026-05-12 00:21:59 +0530

    Update MenuBar.tsx

--- src/components/macos/MenuBar.tsx
+++ src/components/macos/MenuBar.tsx
@@ -340,7 +340,7 @@ export const MenuBar = ({ onSpotlightClick }: MenuBarProps) => {
             </PopoverContent>
           </Popover>
           <button className={`p-1 hover:bg-white/15 rounded ${settings.wifi && online ? '' : 'opacity-50'}`} title={online ? 'Online' : 'Offline'}>
-            <IoIosWifi className="w-4 h-4" />
+            <IoIosWifi className="w-5 h-5" />
           </button>
           <button className="p-1 hover:bg-white/15 rounded" title={`Volume ${settings.volume ?? 65}%`}>
             <Volume2 className="w-4 h-4" />
